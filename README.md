e6e0f221e282fe2b02bcd6ab357b1e43655f842f
Kenny Scott <kenny.scott@pda.wa.gov>
2023-04-20 23:43:08 -0700

    add code and update readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # text2files
 Quickly generates a file and directory structure based on an indented text file
+
+## Notes
+
+1. Make sure your file structure lists any child directory first (before any files on the same level as the parent). This is important because the script will create the directory before creating the file.  If you list files first, the script could fail. 
+
+2. Proper Structure Example:
+    ``` 
+    home
+      user
+        file1.txt
+        file2.txt
+      user2
+        file3.txt
+        file4.txt
+    file5.txt
+    ```
+
+3. INVALID Structure Example:
+    ``` 
+    home
+    file5.txt
+      user
+        file1.txt
+        file2.txt
+      user2
+        file3.txt
+        file4.txt
+    ```
+
+## Usage
+
+1. Create a text file with the desired directory structure.  See the examples above.
+
+2. Run the script with the text file as the first argument.  The second argument is the path to the directory where you want the file structure to be created.  If no second argument is provided, the file structure will be created in the current directory.
+
+**Run command:** `python create_structure.py <path_to_structure_file.txt>`
+
+Example: `python create_structure.py ./documents/structure.txt`
---
+++ files.py
@@ -0,0 +1,81 @@
+# Kenny G Scott - 2023
+# https://github.com/soodohcool
+
+# Standard library imports
+import os
+import sys
+
+# This function creates a directory structure based on the provided text structure
+def create_structure(structure_text):
+    # Split the text structure into separate lines
+    lines = structure_text.splitlines()
+
+    # Create an empty list to act as a stack
+    stack = []
+
+    # Loop through each line in the text structure
+    for line in lines:
+        # Remove any whitespace at the end of the line
+        line = line.rstrip()
+
+        # If the line is empty, skip to the next line
+        if not line:
+            continue
+
+        # Determine the amount of indentation on the line
+        indent = len(line) - len(line.lstrip())
+
+        # Remove any whitespace at the beginning and end of the line
+        line = line.strip()
+
+        # Remove any elements from the stack that have a greater or equal indentation
+        while stack and stack[-1][1] >= indent:
+            stack.pop()
+
+        # If the line contains a dot (.) character, it represents a file
+        if '.' in line:
+            # Construct the file path by joining the directory names in the stack with the file name
+            file_path = os.path.join(*(dir_name for dir_name, _ in stack), line)
+
+            # Get the parent directory of the file path
+            parent_dir = os.path.dirname(file_path)
+
+            # If the parent directory exists, create it. Otherwise, do nothing
+            if parent_dir:
+                os.makedirs(parent_dir, exist_ok=True)
+
+            # Create the file and leave it empty
+            with open(file_path, 'w') as f:
+                pass
+        else:
+            # If the line does not contain a dot (.) character, it represents a directory
+
+            # Append the directory name and its indentation level to the stack
+            stack.append((line, indent))
+
+            # Construct the directory path by joining the directory names in the stack
+            dir_path = os.path.join(*(dir_name for dir_name, _ in stack))
+
+            # Create the directory if it does not exist
+            os.makedirs(dir_path, exist_ok=True)
+
+# This function reads in the contents of a file and returns them as a string
+def read_structure_from_file(file_name):
+    with open(file_name, 'r') as f:
+        return f.read()
+
+# If this script is run directly (not imported as a module)
+if __name__ == '__main__':
+    # If the script is not provided with a file name argument, print usage instructions and exit
+    if len(sys.argv) < 2:
+        print('Usage: python create_structure.py <structure_file>')
+        sys.exit(1)
+
+    # Get the file name from the script argument
+    file_name = sys.argv[1]
+
+    # Read the contents of the file
+    structure_text = read_structure_from_file(file_name)
+
+    # Create the directory structure based on the contents of the file
+    create_structure(structure_text)
